7b454ad85a3a488a628a6f0fcacd86d3bbb7ecb1
iusmani <iusmani@anl.gov>
2020-02-27 11:25:15 -0600

    new file:   docs/Names-Active-Dir-Settings-APS.docx : Document on how to hook Argonne Active directory server to glassifsh server for authentication. new file:   docs/Names-Active-Dir-Settings-APS.pdf : Document on how to hook Argonne Active directory server to glassifsh server for authentication.

--- data/patch-1.sql
+++ data/patch-1.sql
@@ -1,7 +1,4 @@
 -- This patch is to modify category names:
--- device-type to component-function
--- subsystem to primary-system
--- system to primary-machine-location
 
 -- Increase size of column id in table name_category
 ALTER TABLE name_category MODIFY id VARCHAR(32);
@@ -17,6 +14,8 @@ update name_event set name_category_id="primary-machine" where name_category_id=
 update name_category set id="primary-machine", name="Primary Machine/Sub-Area", description="Primary Machine/Sub-Area", version=0 where id='system';
 update name_event set name_category_id="system" where name_category_id='subsystem';
 update name_category set id="system", name="System", description="System", version=0 where id='subsystem';
+
+-- Delete category names only if there is no relative name event present.
 delete from name_category where id='signal-type' and not exists(select 1 from name_event where name_category_id='signal-type');
 delete from name_category where id='signal-suffix' and not exists(select 1 from name_event where name_category_id='signal-suffix');
 delete from name_category where id='signal-domain' and not exists(select 1 from name_event where name_category_id='signal-domain');
